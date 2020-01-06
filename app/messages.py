b788165ba968cd6968545b56df31ab40ceaa8d33
NicUll <lordtardus@gmail.com>
2020-01-06 23:21:08 +0100

    Removed hardcoded table name

--- app/messages.py
+++ app/messages.py
@@ -2,7 +2,7 @@ import re
 import sqlite3
 
 create_messages_table_query = '''
-CREATE TABLE IF NOT EXISTS messages (
+CREATE TABLE IF NOT EXISTS %s (
                                         id integer PRIMARY KEY,
                                         data text NOT NULL,
                                         message text,
@@ -12,32 +12,32 @@ CREATE TABLE IF NOT EXISTS messages (
 
 
 class Messages(object):
-
     '''Class used to handle the messages fetched from Twitch IRC'''
 
     def __init__(self, db_file):
         self.db_file = db_file
+        self.table_name = 'messages'
         self.connection = sqlite3.connect(self.db_file)
-
-    def setup_database(self):
-        cursor = self.connection.cursor()
-        cursor.execute(create_messages_table_query)
-        cursor.close()
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(create_messages_table_query % self.table_name)
 
     def close_connection(self):
         self.connection.close()
 
     def insert_message(self, message):
         m = re.match(r':(.*)!.*#(.*)\s:(.*)', message)
-        if m and len(m.groups() == 3):
+        if m and len(m.groups()) == 3:
             user = m.group(1)
             channel = m.group(2)
             m_data = m.group(3)
             cursor = self.connection.cursor()
             cursor.execute('insert into messages(message, data, user, channel) values (?,?,?,?)', (message, m_data,
-            user, channel))
+                                                                                                   user, channel))
         return
 
-    def get_message(self):
-        pass
+    def get_messages(self):
+        messages = self.cursor.execute("select * from messages")
+        for row in messages:
+            print(row)
+
 
