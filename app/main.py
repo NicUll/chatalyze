2d83f005db110994e14b5e81b037b43461b9b66d
NicUll <lordtardus@gmail.com>
2020-01-07 22:02:27 +0100

    Started building schema, queries and mutations

--- app/main.py
+++ app/main.py
@@ -9,8 +9,10 @@ Part of setup in main.py credit Sergio Lucero: https://gist.github.com/sergioluc
 import datetime
 import os
 
+import graphene
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -24,7 +26,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 
-class Channels(db.Model):
+class Channel(db.Model):
     __tablename__ = 'channels'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -32,8 +34,8 @@ class Channels(db.Model):
     active = db.Column(db.Boolean, nullable=False)
     update_time = db.Column(db.DateTime, default=datetime.now)
     mood = db.Column(db.Integer, db.ForeignKey('mood.id'))
-    messages_per_min = db.Column(db.Integer)
-    messages_per_sec = db.Column(db.Integer)
+    messages_per_min = db.Column(db.Integer, default=0)
+    messages_per_sec = db.Column(db.Integer, default=0)
 
     def __repr__(self):
         return 'Channel #%s' % self.name
@@ -43,8 +45,49 @@ class Mood(db.Model):
     __tablename__ = 'mood'
 
     id = db.column(db.Integer, primary_key=True)
-    desc = db.column(db.String, unique=True)
+    name = db.column(db.String, unique=True)
     rules = db.column(db.String)
 
     def __repr__(self):
-        return 'Mood %i = %s' % (self.id, self.desc)
+        return 'Mood %i = %s' % (self.id, self.name)
+
+
+# Schema
+class ChannelObject(SQLAlchemyObjectType):
+    class Meta:
+        model = Channel
+        interfaces = (graphene.relay.Node,)
+
+
+class MoodObject(SQLAlchemyObjectType):
+    class Meta:
+        model = Mood
+        interfaces = (graphene.relay.Node,)
+
+
+class Query(graphene.ObjectType):
+    node = graphene.relay.Node.Field()
+    all_channels = SQLAlchemyConnectionField(ChannelObject)
+    all_moods = SQLAlchemyConnectionField(MoodObject)
+    
+class AddChannel(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        active = graphene.Boolean(required=True)
+        mood_id = graphene.Integer(required=False)
+        
+    channel = graphene.Field(lambda: ChannelObject)
+    
+    def mutate(self, info, name, active, mood_id):
+        mood = Mood.query.filter_by(id=mood_id).first()
+        channel = Channel(name=name, active=active)
+
+        if mood is not None:
+            channel.mood = mood
+
+        db.session.add(channel)
+        db.session.commit()
+
+        return AddChannel(channel=channel)
+
+        
