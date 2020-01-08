61861f70c90c4c35cd455314b84c70d2e32bdc16
NicUll <lordtardus@gmail.com>
2020-01-08 12:51:07 +0100

    Added PING support Made class more DRY

--- data/connection_settings.py
+++ data/connection_settings.py
@@ -1,3 +1,5 @@
-TWITCH_HOST = 'irc.twitch.tv'
-TWITCH_PORT = 6667
+HOST = 'irc.twitch.tv'
+PORT = 6667
 ENCODING = 'UTF-8'
+PING_MESSAGE = 'PING :tmi.twitch.tv'
+PONG_MESSAGE = 'PONG :tmi.twitch.tv'
