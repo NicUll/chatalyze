c0b7617190a42eb266c724406bf638eb8ffcaad5
NicUll <lordtardus@gmail.com>
2020-01-10 13:04:38 +0100

    Cleaning up messages

--- app/messages.py
+++ app/messages.py
@@ -21,8 +21,8 @@ user_table = [
 class Messages:
     """Class used to handle the messages fetched from Twitch IRC"""
 
-    def __init__(self, irc):
-        self.irc: IRC = irc
+    def __init__(self):
+        self.irc: IRC = IRC()
         self.channel: str = ""
         self.db: DBHandler = DBHandler()
         self.__database: str = ":memory:"
@@ -30,35 +30,44 @@ class Messages:
         self.__user_table: str = "user"
         auth.set_credentials(auth.creds.NICK, auth.creds.OAUTH)
 
-    def connect_irc(self, HOST, PORT):
+    def _connect_irc(self, HOST, PORT, NICK, OAUTH):
         self.irc.connect(HOST, PORT)
-        self.irc.authenticate(auth.get_credentials())
+        if NICK and OAUTH:
+            self.irc.authenticate(OAUTH, NICK)
 
-    def connect_db(self):
+    def _connect_db(self):
         self.db.connect(self.__database)
         self.db.create_table(self.__user_table, user_table)
 
-    def connect(self, connection_settings):
-        self.connect_irc(connection_settings.HOST, connection_settings.PORT)
-        self.connect_db()
+    def connect(self, HOST, PORT, NICK=None, OAUTH=None):
+        self._connect_irc(HOST, PORT, NICK, OAUTH)
+        self._connect_db()
 
-    def set_channel(self, channel: str):
+    def set_channel(self, channel, waitlines):
         if self.channel:
             self.irc.part_channel(self.channel)
         self.channel = channel
         self.__message_table = 'ch_%s_messages' % self.channel
-        self.irc.join_channel(self.channel, wait=7)
+        self.irc.join_channel(self.channel, wait=waitlines)
 
-    @staticmethod
-    def get_message_data_dict(message):
-        m = re.match(r':.*!(?P<user>.*)#(?P<channel>.*)\s:(?P<data>.*)', message)
-        if m and len(m.groups() == 3):
-            return {'user': m.group('user'), 'channel': m.group('channel'), 'data': m.group('data')}
-        return None
-
-    def insert_message(self, message):
-        message_data = Messages.get_message_data_dict(message)
+    def store_message(self, message):
+        message_data = get_message_data_dict(message)
         if message_data:
             self.db.run_sql(f'insert into messages(message, data, user, channel) values (?,?,?,?)', message,
                             message_data['data'], message_data['user'], message_data['channel'])
         return
+
+    def get_message(self):
+        return self.irc.get_data()
+
+    def get_and_store_message(self):
+        message = self.get_message()
+        if message:
+            self.store_message()
+
+
+def get_message_data_dict(message):
+    m = re.match(r':.*!(?P<user>.*)#(?P<channel>.*)\s:(?P<data>.*)', message)
+    if m and len(m.groups() == 3):
+        return {'user': m.group('user'), 'channel': m.group('channel'), 'data': m.group('data')}
+    return None
---
+++ examples/read_store_message.py
@@ -0,0 +1,3 @@
+from app.messages import Messages
+
+messages = Messages()
